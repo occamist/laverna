8282ea8470e167610a48953c4b0f454fa9f6883b
Talha Altınel <talhaaltinel@hotmail.com>
2025-08-17 22:52:54 +0100

    simplify error messages

--- main.go
+++ main.go
@@ -25,27 +25,31 @@ func main() {
 
 	raw, err := os.ReadFile(*filenamePath)
 	if err != nil {
-		log.Fatalf("[ERR] failed to read filename path: %v", err)
+		log.Fatalf("failed to read file(%q): %v", *filenamePath, err)
+	}
+
+	isYAML := strings.HasSuffix(*filenamePath, ".yaml") || strings.HasSuffix(*filenamePath, ".yml")
+	isCSV := strings.HasSuffix(*filenamePath, ".csv")
+	if !isYAML && !isCSV {
+		log.Fatalf("file format must be yaml/yml or csv")
 	}
 
 	var opts []synthesize.Opt
-	if strings.HasSuffix(*filenamePath, ".yaml") || strings.HasSuffix(*filenamePath, ".yml") {
+	if isYAML{
 		opts, err = synthesize.UnmarshalYAML(raw)
 		if err != nil {
-			log.Fatalf("[ERR] failed to unmarshal YAML: %v", err)
+			log.Fatalf("failed to unmarshal YAML: %v", err)
 		}
-	} else if strings.HasSuffix(*filenamePath, ".csv") {
+	}
+	if isCSV {
 		opts, err = synthesize.UnmarshalCSV(raw)
 		if err != nil {
-			log.Fatalf("[ERR] failed to unmarshal CSV: %v", err)
+			log.Fatalf("failed to unmarshal CSV: %v", err)
 		}
-	} else {
-		log.Fatalf("[ERR] file format must be yaml/yml or csv")
-		return
 	}
 
 	runner := synthesize.NewBatchRunner(synthesize.WithMaxWorkers(*maxWorkers))
 	if err := runner.Run(context.Background(), opts); err != nil {
-		log.Fatalf("[ERR] failed to run batch: %v", err)
+		log.Fatalf("failed to run: %v", err)
 	}
 }
