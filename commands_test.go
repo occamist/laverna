56ccb9dd57dd35e21b4c14b04d06e5287ebef5ca
Talha Altinel <talhaaltinel@hotmail.com>
2025-09-15 01:31:07 +0100

    tidy

--- commands_test.go
+++ commands_test.go
@@ -129,15 +129,16 @@ func TestAnkiCmd(t *testing.T) {
 			t.Parallel()
 			tt.filename = tt.setup(t, tt.filename)
 
-			err := ankiCmd(t.Context(), ankiCmdFlags{
+			f := ankiCmdFlags{
 				Filename:   tt.filename,
 				MaxWorkers: maxWorkers,
 				Profile:    tt.profile,
 				Config:     tt.cfg,
-			})
+			}
+			err := ankiCmd(t.Context(), f)
 			if !cmp.Equal(tt.wantErr, err, cmpopts.EquateErrors()) {
 				if !cmp.Equal(strings.Contains(err.Error(), tt.wantErr.Error()), true) {
-					t.Errorf("ankiCmd(%q, %d, %q, %v): wantErr=%v gotErr=%v", tt.filename, maxWorkers, tt.profile, tt.cfg, tt.wantErr, err)
+					t.Errorf("ankiCmd(%v): wantErr=%v gotErr=%v", f, tt.wantErr, err)
 				}
 			}
 		})
@@ -174,14 +175,15 @@ func TestAnkiCmd_FileExtensions(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := ankiCmd(t.Context(), ankiCmdFlags{
+			f := ankiCmdFlags{
 				Filename:   tt.filename,
 				MaxWorkers: maxWorkers,
 				Profile:    profile,
-			})
+			}
+			err := ankiCmd(t.Context(), f)
 			if !cmp.Equal(tt.wantErr, err, cmpopts.EquateErrors()) {
 				if err == nil || !strings.Contains(err.Error(), tt.wantErr.Error()) {
-					t.Errorf("ankiCmd(%q, %d, %q, cfg): wantErr=%v gotErr=%v", tt.filename, maxWorkers, profile, tt.wantErr, err)
+					t.Errorf("ankiCmd(%v): wantErr=%v gotErr=%v", f, tt.wantErr, err)
 				}
 			}
 		})
@@ -195,15 +197,159 @@ func TestAnkiCmd_FileNotFound(t *testing.T) {
 		filename   = "nonexistent.csv"
 	)
 
-	err := ankiCmd(t.Context(), ankiCmdFlags{
+	f := ankiCmdFlags{
 		Filename:   filename,
 		MaxWorkers: maxWorkers,
 		Profile:    profile,
-	})
+	}
+	err := ankiCmd(t.Context(), f)
 	wantErr := errors.New(`failed to open file("nonexistent.csv"): open nonexistent.csv: no such file or directory`)
 	if !cmp.Equal(wantErr, err, cmpopts.EquateErrors()) {
 		if !cmp.Equal(wantErr.Error(), err.Error()) {
-			t.Errorf("ankiCmd(%q, %d, %q): wantErr=%v gotErr=%v", filename, maxWorkers, profile, wantErr, err)
+			t.Errorf("ankiCmd(%v): wantErr=%v gotErr=%v", f, wantErr, err)
+		}
+	}
+}
+
+func TestRunCmd(t *testing.T) {
+	t.Parallel()
+	const (
+		maxWorkers = 20
+	)
+
+	tests := []struct {
+		name     string
+		filename string
+		setup    func(t *testing.T, filename string) string
+		wantErr  error
+	}{
+		{
+			name:     "successful run with CSV file",
+			filename: "synthesize-example.csv",
+			setup: func(t *testing.T, filename string) string { //nolint:thelper // this is inline test helper
+				raw, err := os.ReadFile("testdata/synthesize-example.csv")
+				if err != nil {
+					t.Fatalf("os.ReadFile(): %v", err)
+				}
+				return writeTempFile(t, string(raw), filename)
+			},
+		},
+		{
+			name:     "successful run with YAML file",
+			filename: "synthesize-example.yaml",
+			setup: func(t *testing.T, filename string) string { //nolint:thelper // this is inline test helper
+				raw, err := os.ReadFile("testdata/synthesize-example.yaml")
+				if err != nil {
+					t.Fatalf("os.ReadFile(): %v", err)
+				}
+				return writeTempFile(t, string(raw), filename)
+			},
+		},
+		{
+			name:     "invalid voice",
+			filename: "synthesize-invalid-voice.csv",
+			setup: func(t *testing.T, filename string) string { //nolint:thelper // this is inline test helper
+				raw, err := os.ReadFile("testdata/synthesize-invalid-voice.csv")
+				if err != nil {
+					t.Fatalf("os.ReadFile(): %v", err)
+				}
+				return writeTempFile(t, string(raw), filename)
+			},
+			wantErr: errors.New("no audio line found"),
+		},
+		{
+			name:     "empty CSV file",
+			filename: "empty-synthesize.csv",
+			setup: func(t *testing.T, filename string) string { //nolint:thelper // this is inline test helper
+				return writeTempFile(t, "", filename)
+			},
+			wantErr: errors.New("empty csv"),
+		},
+		{
+			name:     "empty YAML file",
+			filename: "empty-synthesize.yaml",
+			setup: func(t *testing.T, filename string) string { //nolint:thelper // this is inline test helper
+				return writeTempFile(t, "", filename)
+			},
+			wantErr: errors.New("empty yaml"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			tt.filename = tt.setup(t, tt.filename)
+
+			f := runCmdFlags{
+				Filename:   tt.filename,
+				MaxWorkers: maxWorkers,
+			}
+			err := runCmd(t.Context(), f)
+			if !cmp.Equal(tt.wantErr, err, cmpopts.EquateErrors()) {
+				if !cmp.Equal(strings.Contains(err.Error(), tt.wantErr.Error()), true) {
+					t.Errorf("runCmd(%v): wantErr=%v gotErr=%v", f, tt.wantErr, err)
+				}
+			}
+		})
+	}
+}
+
+func TestRunCmd_FileExtensions(t *testing.T) {
+	const maxWorkers = 1
+
+	tests := []struct {
+		name     string
+		filename string
+		wantErr  error
+	}{
+		{
+			name:     "invalid extension yaml",
+			filename: "test.y4ml",
+			wantErr:  errors.New("file format must be yaml/yml or csv"),
+		},
+		{
+			name:     "no extension",
+			filename: "test",
+			wantErr:  errors.New("file format must be yaml/yml or csv"),
+		},
+		{
+			name:     "empty filename",
+			filename: "",
+			wantErr:  errors.New("file format must be yaml/yml or csv"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := runCmdFlags{
+				Filename:   tt.filename,
+				MaxWorkers: maxWorkers,
+			}
+			err := runCmd(t.Context(), f)
+			if !cmp.Equal(tt.wantErr, err, cmpopts.EquateErrors()) {
+				if err == nil || !strings.Contains(err.Error(), tt.wantErr.Error()) {
+					t.Errorf("runCmd(%v): wantErr=%v gotErr=%v", f, tt.wantErr, err)
+				}
+			}
+		})
+	}
+}
+
+func TestRunCmd_FileNotFound(t *testing.T) {
+	const (
+		maxWorkers = 1
+		filename   = "nonexistent.csv"
+	)
+
+	f := runCmdFlags{
+		Filename:   filename,
+		MaxWorkers: maxWorkers,
+	}
+	err := runCmd(t.Context(), f)
+	wantErr := errors.New(`failed to read file("nonexistent.csv"): open nonexistent.csv: no such file or directory`)
+	if !cmp.Equal(wantErr, err, cmpopts.EquateErrors()) {
+		if !cmp.Equal(wantErr.Error(), err.Error()) {
+			t.Errorf("runCmd(%v): wantErr=%v gotErr=%v", f, wantErr, err)
 		}
 	}
 }
